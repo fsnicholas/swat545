7ce4dd743e8d8d032b0e840d6749ea9d6929f6b8
Frank Nicholas <fsnicholas@users.noreply.github.com>
2020-03-08 14:36:19 +1030

    Update toy_line.R
    cleaned up R file

--- toy_line.R
+++ toy_line.R
@@ -5,9 +5,13 @@ x <- runif(40)
 y <- a + b * x + rnorm(40, sd = sqrt(sig_sq))
 (avg_x <- mean(x))
 write(avg_x, "avg_x.txt")
+
 # set margins to print without error
 # Error in plot.new() : figure margins too large
 par(mar=c(1,1,1,1))
+
 plot(x,y)
 abline(a, b, col = "purple")
+
+# create pdf of plot
 dev.print(pdf, "toy_line_plot.pdf")
